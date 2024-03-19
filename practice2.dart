e920d5e57439bb2a2d16c02097d5294624693df2
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-03-19 06:43:17 +0500

    Create practice2.dart

---
+++ practice2.dart
@@ -0,0 +1,25 @@
+
+
+void main(){
+
+  print('Welcome to Dart');
+
+  String name = "Raman";
+  print(name);
+  name = "Khan";
+  print(name);
+
+  var subject = "Maths";
+  print(subject);
+  subject = "English";
+  print(subject);
+
+
+  // Dynamic
+  var section; // dynamic section;
+  section="A";  //string
+  section=1;    //number
+  print(section);
+  section=true; //bool
+  print(section);
+}
