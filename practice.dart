07e7b5d8764233272a74999944ad863748ef2b53
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-03-19 06:43:13 +0500

    Create practice.dart

---
+++ practice.dart
@@ -0,0 +1,29 @@
+
+import 'dart:io';
+
+void main(){
+
+/*
+  print('Welcome to Dart World');
+  stdout.write('Enter your name: ');
+  var name = stdin.readLineSync();
+  print("Welcome, $name");
+
+  var raman = new human()
+*/
+
+  int a=5;
+
+  print(a);
+
+  String name= "Haseeb";
+  print(name);
+
+}
+class human{
+// Constructor
+  human();
+
+
+
+}
