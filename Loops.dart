27757f53cfaf4c629569a0c95672c79cb6ce445f
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-03-24 08:31:38 +0500

    Create Loops.dart

---
+++ Loops.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  print("Loops again n again");
+  for(int i=0; i<10; i++){
+    print("Loops again n again by for loop");
+  }
+
+int no=10;
+  /*do{
+    print("No is: $no");
+  } */
+  while(no<50){
+    print("No is $no");
+    no++;
+  }
+
+}
