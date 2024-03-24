1ef6cc053465e4bec4cb01ce60528db6c3446086
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-03-24 08:31:32 +0500

    Create ConditionalStatements.dart

---
+++ ConditionalStatements.dart
@@ -0,0 +1,16 @@
+void main(){
+  print("object");
+
+  var a = 100;
+  var b = 50;
+  if(a>200 && b>100){
+    print("choti haa dono value");
+  }
+  else if(a<50){
+    print("ni hogi execute");
+  }
+  else if(a>10 || b<60){
+    print("yes now it will execute");
+  }
+}
+
