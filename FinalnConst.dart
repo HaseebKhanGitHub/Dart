6a07bbfe68fa81a5e7d9a0a03642ba6d9e230c85
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-03-24 08:31:35 +0500

    Create FinalnConst.dart

---
+++ FinalnConst.dart
@@ -0,0 +1,26 @@
+
+void main(){
+
+  print("hello world!");
+
+  //declare only one time
+  final String name;
+  name = "raman";
+
+  //Must be declared at initialization
+  //inline
+  const String reg = "Sp21";
+
+  const names= [
+    "raman",
+    "aman",
+    "raju",
+    "rajeev"
+  ];
+  print(names);
+  //names.add("perter");
+
+  //we cannot override it
+  //names=["priya","hira","sana","ayesha"];
+
+}
