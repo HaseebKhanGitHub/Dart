8d5baf93e2bdc22c024c3b50d1c81542edc14f62
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-06-08 23:48:28 +0500

    Create Splitting_Widgets_ui.dart

---
+++ Splitting_Widgets_ui.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+
+        title: Text("Flutter Custom Widget"),
+        backgroundColor: Colors.blue,
+
+      ),
+      body: Container(
+        child: Column(
+          children: [
+
+
+
+
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.indigo,
+                child: ListView.builder(itemBuilder: (context, index) => Padding(
+                    padding:  EdgeInsets.all(11.0),
+                  child: SizedBox(
+                    width: 100,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black,
+                    ),
+                  ),
+                ), itemCount: 10, scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ),
+
+
+            Expanded(
+              flex: 4,
+              child: Container(
+                color: Colors.yellow,
+                child: ListView.builder(itemBuilder:(context, index) => Padding(
+                    padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text('Name'),
+                  subtitle: Text('Mob No'),
+                  trailing: Icon(Icons.delete),
+                ),
+                ),
+                ),
+              ),
+            ),
+
+
+
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.cyan,
+                child: ListView.builder(itemBuilder: (context, index) => Padding(
+                    padding: EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(11),
+                      color: Colors.black
+                    ),
+                  ),
+                ), itemCount: 10, scrollDirection: Axis.horizontal,) ,
+                ),
+              ),
+
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.greenAccent,
+                 child: GridView.count(crossAxisCount: 4,
+                 children: [
+                   Padding(
+                     padding: EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(11),
+                    color: Colors.black,
+                  ),
+                     ),
+                   ),
+
+                   Padding(
+                     padding: EdgeInsets.all(8.0),
+                     child: Container(
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(11),
+                         color: Colors.black,
+                       ),
+                     ),
+                   ),
+
+
+                   Padding(
+                     padding: EdgeInsets.all(8.0),
+                     child: Container(
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(11),
+                         color: Colors.black,
+                       ),
+                     ),
+                   ),
+
+                   Padding(
+                     padding: EdgeInsets.all(8.0),
+                     child: Container(
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(11),
+                         color: Colors.black,
+                       ),
+                     ),
+                   ),
+
+                   Padding(
+                     padding: EdgeInsets.all(8.0),
+                     child: Container(
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(11),
+                         color: Colors.black,
+                       ),
+                     ),
+                   ),
+
+
+                   
+                 ],
+                 ),
+              ),
+            )
+
+
+          ],
+        ),
+      ),
+    );
+
+  }
+}
