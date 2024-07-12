ef2c32b971e6ddc14b75a6774d963a9526417407
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-07-12 23:06:12 +0500

    Create IntroPage2.dart

---
+++ IntroPage2.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:switching_screens/IntroPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: IntroPage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+
+        title: Text('Switching Screens'),
+      ),
+      body: Container(
+        color: Colors.blue,
+      )
+    );
+  }
+}
